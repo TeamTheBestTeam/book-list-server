2e8ecc1350355f0c16a926b3b18dada49d50346c
Jennifer Lawrence <jlawrence010@gmail.com>
2018-05-22 13:39:45 -0700

    loadDB added to help with server connection

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const pg = require( 'pg' );
 const app = express();
 const PORT = process.env.PORT;
 
-const client = new pg.Client( process.env.DATABASE_URL );
+console.log( process.env.DATABASE_URL );
+const client = new pg.Client('postgres://postgres:jplaw@localhost:5432/books_app');
 client.connect();
 client.on( 'error', err => console.error( err ) );
 
@@ -17,5 +18,18 @@ app.get( '/', ( req, res ) => res.send( 'Testing 1, 2, 3' ) );
 
 app.get( '*', ( req, res ) => res.status( 403 ).send( 'This route does not exist' ) );
 
+loadDB()
 app.listen( PORT, () => console.log( `Listening on port: ${PORT}` ) );
 
+function loadDB() {
+  console.log('in load');
+  
+  client.query(
+    `CREATE TABLE IF NOT EXISTS books(
+        book_id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        author VARCHAR(100) NOT NULL,
+        image_url VARCHAR (255),
+        isbn VARCHAR(13));`
+  )
+}
