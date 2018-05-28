c7176aa05ca60a3a0332e5669b759154c5a281ca
Jennifer Lawrence <jlawrence010@gmail.com>
2018-05-27 22:37:30 -0700

    post for form added

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ app.use( cors() );
 
 app.get( '/', ( req, res ) => res.send( 'Testing 1, 2, 3' ) );
 
+app.post( '/api/v1/books', ( request, response ) => {
+  client.query(
+    `
+      INSERT INTO books (title, author, isbn, image_url, description)
+      VALUES ($1, $2, $3, $4, $5);
+    `,
+    [
+      request.body.title,
+      request.body.author,
+      request.body.isbn,
+      request.body.image_url,
+      request.body.description,
+    ]
+  )
+    .then( result => {
+      response.send( 'wahoo it worked.' );
+    } )
+    .catch( err => console.error( err ) );
+} )
+
+
 app.get( '*', ( req, res ) => res.status( 403 ).send( 'This route does not exist' ) );
 
 app.listen( PORT, () => console.log( `Listening on port: ${PORT}` ) );
